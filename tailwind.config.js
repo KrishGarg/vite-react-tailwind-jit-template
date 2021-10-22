e6c167fd70f67f98e01619bf1d859d821f7f87fe
Krish Garg <krishgarg6306@gmail.com>
2021-10-22 05:59:23 +0530

    ✨ Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.jsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
